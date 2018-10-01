bf23b63dbb241a979f77107567e7e38ac842b313
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-30 22:36:14 -0400

    removed the unnecessary dowhile loop

--- WBE_DoCirclesIntersect/Program.cs
+++ WBE_DoCirclesIntersect/Program.cs
@@ -18,35 +18,32 @@ namespace WBE_DoCirclesIntersect
         {
             do
             {
-                do
+                try
                 {
-                    try
-                    {
-                        Console.Clear();
+                    Console.Clear();
 
-                        Console.Write("\nEnter the coordinate of the 1st circle's center\n\n>>> ");
-                        string[] input1 = Console.ReadLine().Trim().Split(',');
-                        double[] center1 = { Convert.ToDouble(input1[0]), Convert.ToDouble(input1[1]) };
-                        Console.Write("\nEnter the radius of the 1st circle\n\n>>> ");
-                        double radius1 = Convert.ToDouble(Console.ReadLine().Trim());
-                        Circle circle1 = new Circle(center1, radius1);
+                    Console.Write("\nEnter the coordinate of the 1st circle's center\n\n>>> ");
+                    string[] input1 = Console.ReadLine().Trim().Split(',');
+                    double[] center1 = { Convert.ToDouble(input1[0]), Convert.ToDouble(input1[1]) };
+                    Console.Write("\nEnter the radius of the 1st circle\n\n>>> ");
+                    double radius1 = Convert.ToDouble(Console.ReadLine().Trim());
+                    Circle circle1 = new Circle(center1, radius1);
 
-                        Console.Write("\nEnter the coordinate of the 2st circle's center\n\n>>> ");
-                        string[] input2 = Console.ReadLine().Trim().Split(',');
-                        double[] center2 = { Convert.ToDouble(input2[0]), Convert.ToDouble(input2[1]) };
-                        Console.Write("\nEnter the radius of the 2st circle\n\n>>> ");
-                        double radius2 = Convert.ToDouble(Console.ReadLine().Trim());
-                        Circle circle2 = new Circle(center2, radius2);
+                    Console.Write("\nEnter the coordinate of the 2st circle's center\n\n>>> ");
+                    string[] input2 = Console.ReadLine().Trim().Split(',');
+                    double[] center2 = { Convert.ToDouble(input2[0]), Convert.ToDouble(input2[1]) };
+                    Console.Write("\nEnter the radius of the 2st circle\n\n>>> ");
+                    double radius2 = Convert.ToDouble(Console.ReadLine().Trim());
+                    Circle circle2 = new Circle(center2, radius2);
 
-                        DoCirclesIntersect(circle1, circle2);
-                        Console.ReadLine();
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.Clear();
-                        Console.WriteLine(ex.Message);
-                    }
-                } while (true);
+                    DoCirclesIntersect(circle1, circle2);
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine(ex.Message);
+                }
             } while (true);
         }
 
