ff81800b29a1201b2eb51955fcc3377d1cbca7d1
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-09 09:50:20 -0400

    adjusted the DoCirclesIntersect method to output a string instead of printing the output directly

--- WBE_DoCirclesIntersect/Program.cs
+++ WBE_DoCirclesIntersect/Program.cs
@@ -20,13 +20,14 @@ namespace WBE_DoCirclesIntersect
             {
                 try
                 {
+                    // collecting information for the 1st circle
                     Console.Write("\nEnter the coordinate of the 1st circle's center\n\n>>> ");
                     string[] input1 = Console.ReadLine().Trim().Split(',');
                     double[] center1 = { Convert.ToDouble(input1[0]), Convert.ToDouble(input1[1]) };
                     Console.Write("\nEnter the radius of the 1st circle\n\n>>> ");
                     double radius1 = Convert.ToDouble(Console.ReadLine().Trim());
                     Circle circle1 = new Circle(center1, radius1);
-
+                    // collecting information for the 2nd circle
                     Console.Write("\nEnter the coordinate of the 2st circle's center\n\n>>> ");
                     string[] input2 = Console.ReadLine().Trim().Split(',');
                     double[] center2 = { Convert.ToDouble(input2[0]), Convert.ToDouble(input2[1]) };
@@ -34,26 +35,25 @@ namespace WBE_DoCirclesIntersect
                     double radius2 = Convert.ToDouble(Console.ReadLine().Trim());
                     Circle circle2 = new Circle(center2, radius2);
 
-                    DoCirclesIntersect(circle1, circle2);
-                    Console.Write("\nPress Enter to check other circles...");
-                    Console.ReadLine();
-                    Console.Clear();
+                    Console.WriteLine("\n" + DoCirclesIntersect(circle1, circle2));
                 }
                 catch (Exception ex)
                 {
                     Console.Clear();
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("\n" + ex.Message);
                 }
+                Console.Write("\nPress Enter to try again...");
+                Console.ReadLine();
+                Console.Clear();
             } while (true);
         }
 
-        private static void DoCirclesIntersect(Circle c1, Circle c2)
+        private static string DoCirclesIntersect(Circle c1, Circle c2)
         {
+            // Nested Ternary operator expression
             double dist = Math.Sqrt(Math.Pow(c1.GetCenter()[1] - c2.GetCenter()[1], 2) + Math.Pow(c1.GetCenter()[0] - c2.GetCenter()[0], 2));
-            Console.WriteLine("\n" + (dist == c1.GetRadius() + c2.GetRadius() || dist == Math.Abs(c1.GetRadius() - c2.GetRadius()) ? "Thecicles are tangent to each other" : 
-                                     (dist < c1.GetRadius() + c2.GetRadius() && dist > Math.Abs(c1.GetRadius() - c2.GetRadius())? 
-                                     "The circles intersect" : "The cicles do not intersect and are not tangent to one another")) 
-                             );
+            return (dist == c1.GetRadius() + c2.GetRadius() || dist == Math.Abs(c1.GetRadius() - c2.GetRadius()) ? "Thecicles are tangent to each other" : 
+                   (dist < c1.GetRadius() + c2.GetRadius() && dist > Math.Abs(c1.GetRadius() - c2.GetRadius())? "The circles intersect" : "The cicles do not intersect and are not tangent to one another"));
         }
     }
 }
